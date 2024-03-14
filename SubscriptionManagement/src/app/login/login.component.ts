23a223745d7b47d1f4ef67cb5366211e4dcb79da
Omkar <omkarkanase2003@gmail.com>
2024-03-14 17:44:17 +0530

    localstorage

--- SubscriptionManagement/src/app/login/login.component.ts
+++ SubscriptionManagement/src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { RouterEvent, RouterLink } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { SubscriptionService } from '../subscription.service';
 import { HttpClientModule } from '@angular/common/http';
+import { LocalService } from '../local.service';
 
 @Component({
   selector: 'app-login',
@@ -15,25 +16,31 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class LoginComponent {
    
-  constructor(private loginservice : SubscriptionService){}
+  constructor(private loginservice : SubscriptionService , private localservice : LocalService){}
 
   userId='';
   password='';
 
   login(){
+    if(this.userId == '' || this.password == ''){
+        alert("Please enter something");
+    }
+    else{
       this.loginservice.login(this.userId, this.password).subscribe(
         (response)=>{
           if(response==1){
             alert("Login Success");
+            this.localservice.setItem("userId", this.userId);
           }
           if(response==2){
             alert("Password Invalid");
           }
-          else{
+          if(response==3){
             alert("InValid Credentials");
           }
         }
       )
+    }
   }
 
 }

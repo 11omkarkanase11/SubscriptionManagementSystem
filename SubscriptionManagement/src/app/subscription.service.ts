d6648a24d411f9720143a275b1db63735e1cc2f5
Omkar <omkarkanase2003@gmail.com>
2024-03-15 14:26:57 +0530

    signup

--- SubscriptionManagement/src/app/subscription.service.ts
+++ SubscriptionManagement/src/app/subscription.service.ts
@@ -7,7 +7,8 @@ import { Observable } from 'rxjs';
 })
 export class SubscriptionService {
   
-  url ="http://localhost:8080/sakshi"
+  url ="http://192.168.5.107:8080/sakshi"
+  url1="http://192.168.5.104:8080/omkar"
   constructor(private http: HttpClient) { }
 
   login(userId:any, password:any):Observable<any>{
@@ -18,4 +19,15 @@ export class SubscriptionService {
   displayUserPlans(userId:any):Observable<any>{
     return this.http.get<any>(`${this.url}/getuserplans/${userId}`);
   }
+
+  singup(userId:any, password:any, email:any, name:any):Observable<any>{
+    const dto={
+      userId:userId,
+      password:password,
+      email:email,
+      name:name
+    };
+   return this.http.post<any>(`${this.url1}/signup`,dto);
+  }
+
 }

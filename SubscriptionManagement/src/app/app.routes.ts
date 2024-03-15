de804f45657e6ec2b05e88cac711d009d0b5eba5
Sakshi Bele <sakshibele2003@gmail.com>
2024-03-15 14:29:47 +0530

    conflicts resolved

--- SubscriptionManagement/src/app/app.routes.ts
+++ SubscriptionManagement/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SignupComponent } from './signup/signup.component';
+import { UpgradedplansComponent } from './upgradedplans/upgradedplans.component';
 
 
 export const routes: Routes = [
     {path:'login', component:LoginComponent},
     {path:'dashboard', component:DashboardComponent},
-    {path:'signup', component:SignupComponent}
+    {path:'signup', component:SignupComponent},
+    {path:'upgradedplans', component:UpgradedplansComponent}
 ];

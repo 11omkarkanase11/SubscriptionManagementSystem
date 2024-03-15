301188cbe48fa3f2cf997092df4b83b6eb37f02e
Sakshi Bele <sakshibele2003@gmail.com>
2024-03-15 14:25:49 +0530

    upgradeplans component added

--- SubscriptionManagement/src/app/app.routes.ts
+++ SubscriptionManagement/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UpgradedplansComponent } from './upgradedplans/upgradedplans.component';
 
 export const routes: Routes = [
     {path:'login', component:LoginComponent},
-    {path:'dashboard', component:DashboardComponent}
+    {path:'dashboard', component:DashboardComponent},
+    {path:'upgradedplans', component:UpgradedplansComponent}
 ];

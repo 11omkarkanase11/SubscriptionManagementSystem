c215a6a0eab47cd8615eb422382cac6564cdc1df
Sakshi Bele <sakshibele2003@gmail.com>
2024-04-16 12:02:03 +0530

    css added to dashboard and add plans

--- SubscriptionManagement/src/app/subscription.service.ts
+++ SubscriptionManagement/src/app/subscription.service.ts
@@ -7,7 +7,10 @@ import { Observable } from 'rxjs';
 })
 export class SubscriptionService {
   
-  url ="http://192.168.5.107:8080/sakshi"
+  // url ="http://192.168.5.104:8081/sakshi"
+  // url1="http://192.168.5.104:8080/omkar"
+
+    url ="http://192.168.5.107:8080/sakshi"
   url1="http://192.168.5.107:8081/omkar"
   constructor(private http: HttpClient) { }
 
